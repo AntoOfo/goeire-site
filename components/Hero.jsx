e5bf8714c64eb6b81478abb50324f278aa9138cb
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-12 22:22:22 +0100

    hero image implemented with overlay and linear gradient

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,3 +1,20 @@
+import HeroImg from "../images/hero.jpg";
+
 export default function Hero() {
-  return <h1>Im the hero lol</h1>;
+  return (
+    <section className="relative w-full h-screen">
+      <div
+        className="absolute inset-0 w-full h-full bg-cover bg-center bg-overlay/30 bg-blend-overlay"
+        style={{
+          backgroundImage: `
+            linear-gradient(to right, rgba(0,0,0,0.34) 34%, rgba(0,0,0,0) 100%),
+            url(${HeroImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "50% 85%",
+        }}
+      ></div>
+
+      <div className="relative z-10"></div>
+    </section>
+  );
 }
