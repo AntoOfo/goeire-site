095a4e350733b8a89a6aca05d2417d6bf5fa5ab0
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-12 22:40:54 +0100

    components of hero added and roughly styled

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import HeroImg from "../images/hero.jpg";
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-screen">
+    <section className="relative w-full h-screen px-hori">
       <div
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-overlay/30 bg-blend-overlay"
         style={{
@@ -14,7 +14,19 @@ export default function Hero() {
         }}
       ></div>
 
-      <div className="relative z-10"></div>
+      <div className="relative z-10 text-white font-manrope">
+        <p className="font-normal text-[20px]">Meet Ireland</p>
+        <h1 className="font-extrabold text-[64px]">
+          From Cliffs to Castles, Experience It All
+        </h1>
+        <p className="font-normal text-[20px]">
+          Plan your perfect escape, from rugged coastlines to charming villages,
+          and make memories that last a lifetime.
+        </p>
+        <button className="px-6 py-1 bg-transparent rounded-md-btn border-2 border-white font-normal text-[20px]">
+          Explore Now
+        </button>
+      </div>
     </section>
   );
 }
