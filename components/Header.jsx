093d3beae3d3d95bb7e841b6b468f070048c4f21
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 18:06:17 +0100

    header ui configured, sits on hero now

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,17 +4,23 @@ import Account from "../icons/user.png";
 
 export default function Header() {
   return (
-    <header className="px-hori flex bg-black font-manrope text-white font-light">
-      <img src={Logo} alt="Site Logo" className="w-[90px] h-[26px]" />
+    <header className="absolute top-0 left-0 z-20 w-full px-hori py-[21px] flex items-center justify-between">
+      <div>
+        <img src={Logo} alt="Site Logo" className="w-[90px] h-[26px]" />
+      </div>
 
-      <p>Home</p>
-      <p>Destinations</p>
-      <p>Experiences</p>
-      <p>Guide</p>
-      <p>Plan Trip</p>
+      <div className="flex gap-[50px] font-manrope text-[20px] text-white font-light">
+        <p>Home</p>
+        <p>Destinations</p>
+        <p>Experiences</p>
+        <p>Guide</p>
+        <p>Plan Trip</p>
+      </div>
 
-      <img src={Search} alt="Search" className="w-6 h-6" />
-      <img src={Account} alt="Account" className="w-6 h-6 " />
+      <div className="flex gap-[50px]">
+        <img src={Search} alt="Search" className="w-6 h-6" />
+        <img src={Account} alt="Account" className="w-6 h-6 " />
+      </div>
     </header>
   );
 }
