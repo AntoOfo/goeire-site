767f174df3d1d1acaa334f90178b05ba09342bf7
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 21:45:06 +0100

    swiper implemented, need to style further

--- components/Destinations.jsx
+++ components/Destinations.jsx
@@ -1,3 +1,11 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { destinationsData } from "../data/destinationsData";
+
 export default function Destinations() {
   return (
     <section className="px-hori py-vert bg-section-main font-manrope">
@@ -6,6 +14,31 @@ export default function Destinations() {
         <p className="font-medium text-[20px]">
           Explore Ireland's most unforgettable locations
         </p>
+      </div>
+
+      <div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          {destinationsData.map((dest) => (
+            <SwiperSlide>
+              <img src={dest.image} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
