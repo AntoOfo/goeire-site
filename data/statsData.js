0c9b77b8c78a0ea832432902914acc0fda67fc79
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 18:19:52 +0100

    data for stats populated

---
+++ data/statsData.js
@@ -0,0 +1,25 @@
+export const statsData = [
+    {
+        value: 7.2,
+        label: "Annual Visitors",
+        decimals: 1
+    },
+
+    {
+        value: 300,
+        label: "Stunning Cliffs",
+        decimals: 0
+    },
+
+    {
+        value: 3200,
+        label: "Historic Castles",
+        decimals: 0
+    },
+
+    {
+        value: 450,
+        label: "Walking Trails",
+        decimals: 0
+    }
+]
