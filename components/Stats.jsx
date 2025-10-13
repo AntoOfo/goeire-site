e0c001fc67f61bc265306128a1823c7a3e572074
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 18:33:29 +0100

    structure of stats section sorted with hardcoded values that'll be removed

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,3 +1,24 @@
 export default function Stats() {
-  return <h1>Stats</h1>;
+  return (
+    <section className="px-hori py-vert bg-section-secondary font-manrope">
+      <div className="flex justify-between">
+        <div className="text-center">
+          <h2 className="font-semibold text-5xl">7.2M+</h2>
+          <h3 className="font-light text-[32px]">Annual Visitors</h3>
+        </div>
+        <div className="text-center">
+          <h2 className="font-semibold text-5xl">300+</h2>
+          <h3 className="font-light text-[32px]">Stunning Cliffs</h3>
+        </div>
+        <div className="text-center">
+          <h2 className="font-semibold text-5xl">3200+</h2>
+          <h3 className="font-light text-[32px]">Historic Castles</h3>
+        </div>
+        <div className="text-center">
+          <h2 className="font-semibold text-5xl">450+</h2>
+          <h3 className="font-light text-[32px]">Walking Trails</h3>
+        </div>
+      </div>
+    </section>
+  );
 }
