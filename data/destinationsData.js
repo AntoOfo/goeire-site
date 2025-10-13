b8c3a53b7cb390fc9bce872d28915b5dbc54eb4a
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 21:04:15 +0100

    destination images added and data file created & populated

---
+++ data/destinationsData.js
@@ -0,0 +1,44 @@
+import Causeway from "../images/causeway.webp"
+import Dublin from "../images/dublin.webp"
+import Galway from "../images/galway.webp"
+import Kerry from "../images/kerry.webp"
+import Kilkenny from "../images/kilkenny.webp"
+import Moher from "../images/moher.webp"
+
+export const destinationsData = [
+    {
+        place: "Dublin",
+        tag: "The Heart of Ireland",
+        image: Dublin
+    },
+
+    {
+        place: "Giant's Causeway",
+        tag: "Nature's Geometric Wonder",
+        image: Causeway
+    },
+    
+    {
+        place: "Galway",
+        tag: "Arts, Music & Seaside Charm",
+        image: Galway
+    },
+
+    {
+        place: "Kerry",
+        tag: "Wild Beauty of the Southwest",
+        image: Kerry
+    },
+
+    {
+        place: "Cliffs of Moher",
+        tag: "Ireland's Iconic Edge",
+        image: Moher
+    },
+
+    {
+        place: "Kilkenny",
+        tag: "Medieval charm with a lively spirit",
+        image: Kilkenny
+    }
+]
