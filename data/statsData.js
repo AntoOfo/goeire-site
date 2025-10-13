1385d568c7c3321e4b147658fd7df475c401cb95
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 18:36:16 +0100

    end values added to each data entry

--- data/statsData.js
+++ data/statsData.js
@@ -2,24 +2,28 @@ export const statsData = [
     {
         value: 7.2,
         label: "Annual Visitors",
-        decimals: 1
+        decimals: 1,
+        end: "M+"
     },
 
     {
         value: 300,
         label: "Stunning Cliffs",
-        decimals: 0
+        decimals: 0,
+        end: "+"
     },
 
     {
         value: 3200,
         label: "Historic Castles",
-        decimals: 0
+        decimals: 0,
+        end: "+"
     },
 
     {
         value: 450,
         label: "Walking Trails",
-        decimals: 0
+        decimals: 0,
+        end: "+"
     }
 ]
