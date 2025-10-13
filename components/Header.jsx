43c8f1b5ce589989482128c3ad1e5a503b91c31c
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 17:38:47 +0100

    components added to header, need to structure

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,20 @@
+import Logo from "../icons/eirelogo.png";
+import Search from "../icons/search.png";
+import Account from "../icons/user.png";
+
 export default function Header() {
-  return <h1>im the header lol</h1>;
+  return (
+    <header className="px-hori flex bg-black font-manrope text-white font-light">
+      <img src={Logo} alt="Site Logo" className="w-[90px] h-[26px]" />
+
+      <p>Home</p>
+      <p>Destinations</p>
+      <p>Experiences</p>
+      <p>Guide</p>
+      <p>Plan Trip</p>
+
+      <img src={Search} alt="Search" className="w-6 h-6" />
+      <img src={Account} alt="Account" className="w-6 h-6 " />
+    </header>
+  );
 }
