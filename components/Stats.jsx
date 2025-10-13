6e9e12d0589d51c15693040ad3aa9f6f2cfcb564
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-13 18:39:29 +0100

    stats data mapped over ui

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,23 +1,18 @@
+import { statsData } from "../data/statsData";
+
 export default function Stats() {
   return (
     <section className="px-hori py-vert bg-section-secondary font-manrope">
       <div className="flex justify-between">
-        <div className="text-center">
-          <h2 className="font-semibold text-5xl">7.2M+</h2>
-          <h3 className="font-light text-[32px]">Annual Visitors</h3>
-        </div>
-        <div className="text-center">
-          <h2 className="font-semibold text-5xl">300+</h2>
-          <h3 className="font-light text-[32px]">Stunning Cliffs</h3>
-        </div>
-        <div className="text-center">
-          <h2 className="font-semibold text-5xl">3200+</h2>
-          <h3 className="font-light text-[32px]">Historic Castles</h3>
-        </div>
-        <div className="text-center">
-          <h2 className="font-semibold text-5xl">450+</h2>
-          <h3 className="font-light text-[32px]">Walking Trails</h3>
-        </div>
+        {statsData.map((stat) => (
+          <div className="text-center">
+            <h2 className="font-semibold text-5xl">
+              {stat.value}
+              {stat.end}
+            </h2>
+            <h3 className="font-light text-[32px]">{stat.label}</h3>
+          </div>
+        ))}
       </div>
     </section>
   );
