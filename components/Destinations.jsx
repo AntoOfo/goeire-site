a1e14eb2507048eeccdd7da1c2c4480103105965
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-14 23:25:37 +0100

    horizontal padding removed so carousel takes full width

--- components/Destinations.jsx
+++ components/Destinations.jsx
@@ -10,7 +10,7 @@ import { destinationsData } from "../data/destinationsData";
 
 export default function Destinations() {
   return (
-    <section className="px-hori py-vert bg-section-main font-manrope">
+    <section className=" py-vert bg-section-main font-manrope">
       <div className=" flex flex-col gap-4 text-center text-primary">
         <h2 className="font-semibold text-5xl">Top Destinations</h2>
         <p className="font-medium text-[20px]">
