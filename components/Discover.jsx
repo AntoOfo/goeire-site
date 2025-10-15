168c81b9bc5cf148935d745921c40d54e15b5a02
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-15 22:29:37 +0100

    header and background added in discover and data file started for the content

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -1,3 +1,9 @@
 export default function Discover() {
-  return <h1>Im the discover section lol</h1>;
+  return (
+    <section className="px-hori py-vert bg-section-secondary font-manrope text-primary">
+      <div>
+        <h2 className="font-semibold text-5xl">What Will You Discover?</h2>
+      </div>
+    </section>
+  );
 }
