a0eb7ac536323658c3e5e4810804278694370877
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-17 18:09:05 +0100

    naviagtion structure started and mui installed

--- components/Navigate.jsx
+++ components/Navigate.jsx
@@ -1,3 +1,19 @@
 export default function Navigate() {
-  return <h1>Navigation section</h1>;
+  return (
+    <section className="px-hori py-vert bg-section-main font-manrope text-primary">
+      <div className="flex">
+        <div>
+          <h2 className="font-semibold text-5xl mb-8">
+            Navigate The Emerald Isle
+          </h2>
+          <p className="font-medium text-[18px] opacity-80 w-[600px] leading-[220%]">
+            Ireland offers breathtaking scenery, deep history, and vibrant
+            culture. From rugged coasts to charming towns, every stop holds
+            something unforgettable. Use this guide to plan and explore its
+            hidden gems.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
