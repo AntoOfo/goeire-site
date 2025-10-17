cfe437c6ebb7bf81542bdeb43fa57a35cebabe4a
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-17 19:06:15 +0100

    new icons brought in and data file populated

--- components/Navigate.jsx
+++ components/Navigate.jsx
@@ -1,17 +1,31 @@
+import Divider from "@mui/material/Divider";
+
+import Clover from "../icons/nature.png";
+
 export default function Navigate() {
   return (
     <section className="px-hori py-vert bg-section-main font-manrope text-primary">
       <div className="flex">
-        <div>
+        <div className="w-[600px]">
           <h2 className="font-semibold text-5xl mb-8">
             Navigate The Emerald Isle
           </h2>
-          <p className="font-medium text-[18px] opacity-80 w-[600px] leading-[220%]">
+          <p className="font-medium text-[18px] opacity-80 leading-[220%] mb-9">
             Ireland offers breathtaking scenery, deep history, and vibrant
             culture. From rugged coasts to charming towns, every stop holds
             something unforgettable. Use this guide to plan and explore its
             hidden gems.
           </p>
+          <Divider className="w-[580px] font-medium text-[18px] text-primary/80">
+            Quick Travel Tips
+          </Divider>
+          <span className="flex items-center gap-3.5 mt-9">
+            <img src={Clover} className="w-[32px] h-[32px]" />
+            <p className="font-medium text-[18px] opacity-80">
+              Best time to visit: May to September for mild weather and long
+              daylight hours
+            </p>
+          </span>
         </div>
       </div>
     </section>
---
+++ data/navigateData.js
@@ -0,0 +1,20 @@
+import Clover from "../icons/nature.png"
+import Suitcase from "../icons/suitcase.png"
+import Relax from "../icons/relax.png"
+
+export const navigateData = [
+    {
+        icon: Clover,
+        desc: "Best time to visit: May to September for mild weather and long daylight hours"
+    },
+
+    {
+        icon: Suitcase,
+        desc: "Pack layers and waterproofs. Irish weather is famously unpredictable"
+    },
+
+    {
+        icon: Relax,
+        desc: "Don't rush.  Ireland is best experienced at a leisurely, exploratory pace"
+    }
+]
