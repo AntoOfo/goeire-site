962f3d0c427e1cf4204b84fd2eb14fefc4d5a306
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-19 00:11:18 +0100

    structure for call to action section

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,3 +1,16 @@
 export default function Cta() {
-  return <h1>cta section</h1>;
+  return (
+    <section className="px-hori py-vert bg-section-secondary font-manrope">
+      <div className="bg-primary w-full flex">
+        <span>
+          <h3>Ready to Explore Ireland?</h3>
+          <p>
+            Start your journey through Ireland and plan your next adventure
+            today.
+          </p>
+        </span>
+        <button>Plan Your Trip</button>
+      </div>
+    </section>
+  );
 }
