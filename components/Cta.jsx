142922b7382deb13fe1107f67b60689bb1e4852c
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-19 16:48:47 +0100

    cta styled

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,15 +1,19 @@
 export default function Cta() {
   return (
     <section className="px-hori py-vert bg-section-secondary font-manrope">
-      <div className="bg-primary w-full flex">
-        <span>
-          <h3>Ready to Explore Ireland?</h3>
-          <p>
+      <div className="bg-primary flex w-full py-7 px-14 rounded-section items-center justify-between">
+        <span className="w-[695px]">
+          <h3 className="font-semibold text-4xl text-white">
+            Ready to Explore Ireland?
+          </h3>
+          <p className="font-medium text-2xl text-white mt-2">
             Start your journey through Ireland and plan your next adventure
             today.
           </p>
         </span>
-        <button>Plan Your Trip</button>
+        <button className="font-medium text-2xl rounded-md-btn bg-white text-primary px-6 py-4">
+          Plan Your Trip
+        </button>
       </div>
     </section>
   );
