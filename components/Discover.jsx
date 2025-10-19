12d28475be2c41e38fc073f003da400bf64639b8
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-19 20:51:41 +0100

    section styled

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -10,25 +10,29 @@ export default function Discover() {
       <div>
         <h2 className="font-semibold text-5xl">What Will You Discover?</h2>
 
-        <div>
-          <div className="flex">
-            <img src={Placeholder} className="w-24 h-24" />
-            <div className="flex justify-between">
-              <div>
-                <span className="flex">
-                  <h3 className="font-medium text-[28px]">Galway Music</h3>
-                  <p className="font-medium text-[16px] opacity-60">
-                    Culture • Galway
+        <div className="mt-14">
+          <div>
+            <Divider />
+            <div className="flex gap-7 items-center py-8">
+              <img src={Placeholder} className="w-32 h-32 rounded-img" />
+              <div className="flex w-full justify-between items-center">
+                <div className="flex flex-col gap-0.5">
+                  <span className="flex gap-5 items-baseline">
+                    <h3 className="font-medium text-[28px]">Galway Music</h3>
+                    <p className="font-medium text-[16px] opacity-60">
+                      Culture • Galway
+                    </p>
+                  </span>
+                  <p className="font-normal text-[20px]">
+                    Experience live music and the energy that fills Galway's
+                    pubs.
                   </p>
-                </span>
-                <p className="font-normal text-[20px]">
-                  Experience live music and the energy that fills Galway's pubs.
-                </p>
+                </div>
+                <img src={Chevron} className="w-4 h-4" />
               </div>
-              <img src={Chevron} className="w-4 h-4" />
             </div>
+            <Divider />
           </div>
-          <Divider />
         </div>
       </div>
     </section>
