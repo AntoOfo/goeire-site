816a0a1ead3da3b298144130f31c8f7d9aef545e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-20 20:02:10 +0100

    inview implemented so countup triggers when section is in view

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,18 +3,30 @@ import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
 
 export default function Stats() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.3,
+  });
+
   return (
-    <section className="px-hori py-vert bg-section-secondary font-manrope text-primary">
+    <section
+      ref={ref}
+      className="px-hori py-vert bg-section-secondary font-manrope text-primary"
+    >
       <div className="flex justify-between">
         {statsData.map((stat) => (
           <div className="text-center">
             <h2 className="font-semibold text-5xl">
-              <CountUp
-                start={0}
-                end={stat.value}
-                duration={3}
-                decimals={stat.decimals}
-              />
+              {inView ? (
+                <CountUp
+                  start={0}
+                  end={stat.value}
+                  duration={3}
+                  decimals={stat.decimals}
+                />
+              ) : (
+                0
+              )}
               {stat.end}
             </h2>
             <h3 className="font-light text-[32px]">{stat.label}</h3>
