2cf9d5833b4664965985e179235c71ffca1e7ece
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-22 16:41:37 +0100

    aos library installed and imported in app jsx

--- App.jsx
+++ App.jsx
@@ -8,7 +8,18 @@ import Testimonials from "./components/Testimonials";
 import Cta from "./components/Cta";
 import Footer from "./components/Footer";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+      once: false,
+    });
+  }, []);
+
   return (
     <>
       <Header />
