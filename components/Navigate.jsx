4b71c21afcbe8461162eb36663c77113e8373253
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-23 23:00:12 +0100

    naviagte section revamped

--- components/Navigate.jsx
+++ components/Navigate.jsx
@@ -1,4 +1,5 @@
 import Divider from "@mui/material/Divider";
+import Pin from "../icons/pin.png";
 
 import { navigateData } from "../data/navigateData";
 import { locationsData } from "../data/locationsData";
@@ -40,19 +41,23 @@ export default function Navigate() {
           <p className="font-medium text-[20px] mb-9">
             Explore Ireland's landscapes and hidden gems
           </p>
-          <Divider className="w-[580px] font-medium text-[18px] text-primary/80">
-            Travel Tips
-          </Divider>
+          <Divider className="w-[580px]" />
+          <div className="flex items-center gap-3.5 mb-9 mt-9">
+            <img src={Pin} className="w-[26px] h-[26px]" />
+            <p className=" font-medium text-[20px] text-primary/80">
+              Travel Tips
+            </p>
+          </div>
           {navigateData.map((nav) => (
-            <span className="flex items-center gap-3.5 mt-9">
+            <div className="flex items-center gap-3.5 mt-7 bg-white border-1 border-primary/30 rounded-img px-4 py-3">
               <img src={nav.icon} className="w-[32px] h-[32px]" />
               <p className="font-medium text-[18px] opacity-80">{nav.desc}</p>
-            </span>
+            </div>
           ))}
         </div>
         <div
           id="map"
-          className="w-[634px] h-[440px] rounded-img z-10 overflow-hidden border-2 border-black/30"
+          className="w-[634px] h-[530px] rounded-img z-10 overflow-hidden border-1 border-black/30"
         ></div>
       </div>
     </section>
