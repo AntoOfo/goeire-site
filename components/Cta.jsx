8c1c8ba6ea819c0b69599de228ba3fba9c2e09b0
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-10-25 21:36:17 +0100

    clamps set up in cta

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -4,17 +4,17 @@ export default function Cta() {
       id="plan"
       className="px-hori py-vert bg-section-secondary font-manrope"
     >
-      <div className="bg-gradient-to-t from-gradienttwo to-primary flex w-full py-7 px-14 rounded-section items-center justify-between">
-        <span className="w-[580px]">
-          <h3 className="font-semibold text-4xl text-white">
+      <div className="bg-gradient-to-t from-gradienttwo to-primary flex w-full py-[clamp(0.88rem,1.84vw,1.75rem)] px-[clamp(1.75rem,3.68vw,3.5rem)] rounded-section items-center justify-between">
+        <span className="w-[clamp(18.13rem,38.13vw,36.25rem)]">
+          <h3 className="font-semibold text-[clamp(1.13rem,2.36vw,2.25rem)] text-white">
             Ready to Explore Ireland?
           </h3>
-          <p className="font-medium text-[20px] text-white/90 mt-2">
+          <p className="font-medium text-[clamp(0.63rem,1.32vw,1.25rem)] text-white/90 mt-[clamp(0.25rem,0.53vw,0.5rem)]">
             Start your journey through Ireland and plan your next adventure
             today.
           </p>
         </span>
-        <button className="font-semibold text-2xl rounded-md-btn bg-transparent border-2 border-white text-white px-7 py-4 duration-300 ease-in-out hover:bg-white hover:text-black hover:cursor-pointer">
+        <button className="font-semibold text-[clamp(0.75rem,1.58vw,1.5rem)] rounded-md-btn bg-transparent border-2 border-white text-white px-[clamp(0.88rem,1.84vw,1.75rem)] py-[clamp(0.5rem,1.05vw,1rem)] duration-300 ease-in-out hover:bg-white hover:text-black hover:cursor-pointer">
           Plan Your Trip
         </button>
       </div>
